5b76916d502e3c8cb7def82d52955dc50c7b382e
Kestutis Kurienius <kurkestutis@gmail.com>
2022-01-14 13:42:32 +0200

    Plural suffixes are added to the names of time units.

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,11 @@ const hourEl = document.getElementById("hourNumberId");
 const minuteEL = document.getElementById("minNumberId");
 const secondEL = document.getElementById("secNumberId");
 
+const dayName = document.getElementById("dayNameId");
+const hourName = document.getElementById("hourNameId");
+const minuteName = document.getElementById("minNameId");
+const secondName = document.getElementById("secNameId");
+
 const calculateTime = () => {
   const target = new Date("1 Jan 2023");
   const timeNow = new Date();
@@ -14,6 +19,22 @@ const calculateTime = () => {
   const minNum = Math.floor((timeDifferenceInSeconds / 60) % 60);
   const secNum = Math.floor(timeDifferenceInSeconds % 60);
   console.log(dayNum, hourNum, minNum, secNum);
+
+  if (dayNum == 1) {
+    dayName.innerHTML = "Day";
+  }
+
+  if (hourNum == 1) {
+    hourName.innerHTML = "Hour";
+  }
+
+  if (minNum == 1) {
+    minuteName.innerHTML == "Minut";
+  }
+
+  if (secNum == 1) {
+    secName.innerHTML == "Second";
+  }
 
   dayEl.innerHTML = dayNum;
   hourEl.innerHTML = hourNum;
